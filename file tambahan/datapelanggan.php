2526ccc70c176e758a985c78cd9cc0a743cbab5e
luthfihanif13 <67750065+luthfihanif13@users.noreply.github.com>
2022-10-20 00:04:08 +0800

    File tambahan(ketinggalan di upload)

---
+++ file tambahan/datapelanggan.php
@@ -0,0 +1,15 @@
+<?php
+if ($_POST) {
+    echo "Nama: " . $_POST['Nama'];
+    echo "<br/>";
+    echo "Email: " . $_POST['Email'];
+    echo "<br/>";
+    echo "Nomor HP: " . $_POST['Nomor HP'];
+    echo "<br/>";
+    echo "Tanggal: " . $_POST['Tanggal'];
+    echo "<br/>";
+    echo "Alamat: " . $_POST['Alamat'];
+
+}
+
+?>
